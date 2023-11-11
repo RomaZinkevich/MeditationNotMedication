51c10d4715b67da3d8e6411fa232678fcb826561
RomaZinkevich <zra1903@mail.ru>
2023-11-11 16:48:25 +0200

    err message in GET /api/section

--- backend/src/api/sections.js
+++ backend/src/api/sections.js
@@ -7,7 +7,7 @@ const pool = require("../db/dbconfig")
 router.get("/", (req, res) => {
     pool.query('SELECT section_name, content_name, image FROM content AS C, section AS S WHERE S.section_id=C.section_id;', (err, result) => {
         if (err)
-            res.status(500).send(err.message);
+            res.status(500).json(err.message);
         else
             res.json(result.rows);
     });
