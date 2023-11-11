1e50035de0c577ee0260fea3607efbe241405db6
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2023-11-11 21:36:58 +0200

    Added redirection functionality on click.

--- frontend/src/components/CardComponent.jsx
+++ frontend/src/components/CardComponent.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "../styles/components/Card.scss";
+import {useLocation} from "react-router-dom";
+
 
 const CardComponent = ({card, image}) => {
+  const location = useLocation();
+
+  const cardStyle = {
+    backgroundImage: `url(${image})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+
+  const handleClick = () => {
+    const newLocation = `/exercises/${card.gId}`;
+    window.location.href = newLocation;
+  };
   return (
-    <div className="insideInfo">
-      <h3>{card.title}</h3>
+    <div className="insideInfo" style={cardStyle} onClick={handleClick}>
+      <h3 className="cardTitle">{card.title}</h3>
       <p>{card.description}</p>
-      <img src={image} alt="Image" />
     </div>
   );
 };
--- frontend/src/components/GuideSection.jsx
+++ frontend/src/components/GuideSection.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useRef} from "react";
 import CardComponent from "./CardComponent";
 
 const GuideSection = ({Cards, sectionID, name}) => {
+  const containerRef = useRef(null);
+
+  const handleScroll = (scrollOffset) => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += scrollOffset;
+    }
+  };
   return (
-    <div>
+    <div className="container">
+      <div>
+        <button onClick={ () => {
+          console.log("Container Ref:", containerRef.current); handleScroll(-200);
+        }}>Scroll Left</button>
+        <button onClick={ () => {
+          console.log("Container Ref:", containerRef.current); handleScroll(200);
+        }}>Scroll Right</button>
+      </div>
       <h2>{name}</h2>
-      <div className="cards_container">
+      <div className="cards_container" ref={containerRef}>
         {Cards.map((card) => (<CardComponent key={card.gId} card={card} image={card.img}/>))}
       </div>
     </div>
