4382909a1efe92ca2a20ba023bbf979676363ec4
Zhiyuan Liu <thenukezealot@gmail.com>
2023-11-12 06:43:03 +0200

    profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import "../styles/components/profile.scss";
 
 function Profile() {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/auth/success")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          setIsLogged(true);
+        }
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+      });
+  }, []);
 
   return (
     <>
       <NavBar />
       {isLogged ? (
-        <h1>Logged In</h1>
+        <>
+          <div className="stats">
+            <h3>
+              Ease Button Clicked: <span>23 times</span>{" "}
+            </h3>
+            <h3></h3>
+          </div>
+        </>
       ) : (
         <>
           <a href="https://meditationnotmedication-production.up.railway.app/auth/google">
