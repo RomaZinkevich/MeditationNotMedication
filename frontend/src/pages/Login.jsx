7c566461487c842bcfa7618740e99503bb8eef83
Zhiyuan Liu <thenukezealot@gmail.com>
2023-11-14 15:33:46 +0200

    login page skeleton

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import logo from "../images/logo.png";
+import GoogleIcon from "@mui/icons-material/Google";
+import { Link } from "react-router-dom";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <button>Skip</button>
+      <div className="logo-wrapper">
+        <img src={logo} alt="" />
+        <h1>EazeEase</h1>
+      </div>
+      <section className="login-options">
+        <button>Continue With Email</button>
+        <button>
+          <GoogleIcon></GoogleIcon>Continue With Google
+        </button>
+      </section>
+      <p>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+    </>
+  );
 }
 
 export default Login;
