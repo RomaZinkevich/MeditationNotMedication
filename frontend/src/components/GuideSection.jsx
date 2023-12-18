2cbdcd37d408e251faf5943a36fab18cbb0473d1
Prabesh Sharma <prwshshrm@gmail.com>
2023-12-18 17:13:42 +0200

    Fixed issues with cards displaying in a wrong sorting order
    Minor refactor for Home.jsx

--- frontend/src/components/GuideSection.jsx
+++ frontend/src/components/GuideSection.jsx
@@ -1,23 +1,38 @@
 import React, { useState, useEffect } from "react";
 import CardComponent from "./CardComponent";
 
-const GuideSection = ({ sectionID, name }) => {
+const GuideSection = ({ sectionID, name }) =>
+{
   const [cards, setCards] = useState([]);
-  useEffect(() => {
+  useEffect(() =>
+  {
     fetch(
       `https://meditationnotmedication-production.up.railway.app/api/sections/${sectionID}`
     )
       .then((res) => res.json())
-      .then((data) => {
+      .then((data) =>
+      {
+        data.sort((a, b) =>
+        {
+          const dayA = extractDayNumber(a.content_name);
+          const dayB = extractDayNumber(b.content_name);
+          return dayA - dayB;
+        })
         setCards(data);
       });
   }, []);
+
+  const extractDayNumber = (contentName) =>
+  {
+    const dayString = contentName.replace("Day ", "");
+    return parseInt(dayString, 10);
+  };
 
   return (
     <div className="container">
       <h2>{name}</h2>
       <div className="cards_container">
-        {cards.map((card) => (
+        {cards.sort().map((card) => (
           <CardComponent key={card.content_id} card={card} />
         ))}
       </div>
