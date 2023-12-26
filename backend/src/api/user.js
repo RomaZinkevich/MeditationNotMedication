6c7d03970b749bbdcb97dfc46677e7b65a86127d
RomaZinkevich <zra1903@mail.ru>
2023-12-26 14:03:02 +0200

    PUT endpoint for /api/users

--- backend/src/api/user.js
+++ backend/src/api/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const checkToken = require("../middleware/auth");
-const { createUser, loginUser } = require("../db/userdb");
+const { createUser, loginUser, changeUser } = require("../db/userdb");
 const { tryCatch } = require("../utils/tryCatch");
 
 const DEFAULT_IMAGE = "https://ih1.redbubble.net/image.1046392292.3346/st,medium,507x507-pad,600x600,f8f8f8.jpg";
@@ -14,7 +14,8 @@ const schema = Joi.object({
     password: Joi.string().min(8)
     .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)
     .required(),
-    email: Joi.string().required()
+    email: Joi.string().required(),
+    image: Joi.string().required()
 });
 
 // @desc Creates new user in the database
@@ -25,13 +26,15 @@ router.post("/",
         let newUser = {
             name: req.body.name,
             password: req.body.password,
-            email: req.body.email
+            email: req.body.email,
+            image: DEFAULT_IMAGE
         }
         const { error, value } = schema.validate(newUser);
         if (error) throw error;
 
-        await createUser(newUser);
+        const response = await createUser(newUser);
         delete newUser.password;
+        newUser.id = response.user_id
         newUser.image = DEFAULT_IMAGE;
 
         let token = jwt.sign(newUser, process.env.JWT_SECRET_KEY, {
@@ -52,6 +55,7 @@ router.post("/login",
         let response = await loginUser(user);
 
         delete user.password;
+        user.id = response.user_id;
         user.name = response.name;
         user.image = response.image;
 
@@ -66,7 +70,31 @@ router.post("/login",
 // @access Private
 router.get("/", checkToken,
     tryCatch(async (req, res, next) => {
-        return res.json({ "status": "success", "data": req.user });
+        return res.json({ "status": "success", "details": req.user });
+}));
+
+// @desc Changes user info
+// @route PUT /api/users
+// @access Private
+router.put("/", checkToken,
+    tryCatch(async (req, res, next) => {
+        let updatedUser = {
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            image: req.body.image
+        }
+        const { error, value } = schema.validate(updatedUser);
+        if (error) throw error;
+
+        await changeUser(updatedUser, req.user);
+        delete updatedUser.password;
+        updatedUser.id = req.user.id;
+        
+        let token = jwt.sign(updatedUser, process.env.JWT_SECRET_KEY, {
+            expiresIn: "10m",
+        });
+        return res.json({"status": "success", "token": token, "details": updatedUser});
 }));
 
 
--- backend/src/db/userdb.js
+++ backend/src/db/userdb.js
@@ -5,9 +5,10 @@ const { encrypt, compare } = require("../utils/passwordEncryption");
 //@desc Creates new user in database
 const createUser = async (newUser) => {
     newUser.password = await encrypt(newUser.password);
-    const query = `INSERT INTO "user" (user_name, email, password) VALUES ('${newUser.name}', '${newUser.email}', '${newUser.password}');`;
+    const query = `INSERT INTO "user" (user_name, email, password) VALUES ($1, $2, $3) RETURNING *;`;
     try {
-        await pool.query(query);
+        const result = await pool.query(query, [newUser.name, newUser.email, newUser.password]);
+        return result.rows[0];
     } catch (error) {
         if (error.message === "duplicate key value violates unique constraint \"email\"")
             throw new UserError("EmailValidationError", "Email already exists");
@@ -17,21 +18,35 @@ const createUser = async (newUser) => {
 
 //@desc Logs in user
 const loginUser = async (user) => {
-    const query = `SELECT user_name AS name, password AS encryptedpassword, image  FROM "user" WHERE email = '${user.email}'`;
+    const query = `SELECT user_id, user_name AS name, password AS encryptedpassword, image  FROM "user" WHERE email = $1`;
     try {
-        const results = await pool.query(query);
+        const results = await pool.query(query, [user.email]);
 
         if (results.rowCount === 0) 
-            throw new UserError("AuthenticationError", "Email doesn't exist")
+            throw new UserError("AuthenticationError", "Email doesn't exist");
 
-        const { name, encryptedpassword, image } = results.rows[0]; 
+        const { user_id, name, encryptedpassword, image } = results.rows[0]; 
         const isPasswordValid = await compare(user.password, encryptedpassword)
 
         if (isPasswordValid) 
-            return { "name": name, "image": image  };
+            return { "name": name, "image": image, "user_id": user_id  };
         throw new UserError("AuthenticationError", "Wrong or no password");
     } catch (error) {
         throw new UserError("AuthenticationError", error.details ? error.details : "Unexpected database error");
+    }
+};
+
+//@desc Change user's data
+
+//FIX: IF QUERY RETURNS EMPTY STILL SUCCESS
+const changeUser = async (updatedUser, user) => {
+    updatedUser.password = await encrypt(updatedUser.password);
+    const query = `UPDATE "user" SET user_name=$1, email=$2, image=$3, password=$4 WHERE user_id=$5`;
+    try {
+        await pool.query(query, [updatedUser.name, updatedUser.email, updatedUser.image, updatedUser.password, user.id]);
+    } catch (error) {
+        console.log(error)
+        throw new UserError("UserDatabaseError", error.details ? error.details : "Unexpected database error");
     }
 };
 
@@ -48,7 +63,8 @@ const clearUsers = async () => {
 
 module.exports = {
     createUser: createUser,
-    clearUsers: clearUsers,
-    loginUser: loginUser
+    changeUser: changeUser,
+    loginUser: loginUser,
+    clearUsers: clearUsers
 };
 
