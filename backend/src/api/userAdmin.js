12cffd77606927d5ea5967baa8e691e020ef859a
RomaZinkevich <zra1903@mail.ru>
2024-01-08 15:24:23 +0200

    PUT endpoint for changing users` roles added

--- backend/src/api/userAdmin.js
+++ backend/src/api/userAdmin.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const { checkToken, adminToken } = require("../middleware/auth");
-const { getAllUsers } = require("../db/userdb");
+const { getAllUsers, changeUserRole } = require("../db/userdb");
 const { tryCatch } = require("../utils/tryCatch");
 
 const router = express.Router();
 
 // @desc Gets info about all users
 // @route GET /api/users/admin
-// @access Private
+// @access Private (Admin)
 router.get("/", checkToken, adminToken,
     tryCatch(async (req, res, next) => {
         let response = await getAllUsers();
+        return res.json({ "status": "success", "details": response });
+}));
+
+// @desc Change role for one of the users
+// @route PUT /api/users/admin/:id
+// @access Private (Admin)
+router.put("/:id", checkToken, adminToken,
+    tryCatch(async (req, res, next) => {
+        let response = await changeUserRole(req.body.role, req.params.id);
         return res.json({ "status": "success", "details": response });
 }));
 
