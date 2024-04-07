d5bfe5518aa80bf9e470ef5dab3155f6818d5132
Zhiyuan Liu <thenukezealot@gmail.com>
2024-04-07 12:40:43 +0300

    home page basic restyling for mobile :

--- frontend_p/.eslintrc.cjs
+++ frontend_p/.eslintrc.cjs
@@ -1,24 +1,24 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "quotes": [2, "double", { "avoidEscape": true }],
-    "require-jsdoc": 0,
-    "linebreak-style": 0,
-    "react/prop-types": 0,
-    "max-len": 0,
-    "no-unused-vars": 0,
-  },
-}
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "plugin:react-hooks/recommended",
+//   ],
+//   ignorePatterns: ["dist", ".eslintrc.cjs"],
+//   parser: "@typescript-eslint/parser",
+//   plugins: ["react-refresh"],
+//   rules: {
+//     "react-refresh/only-export-components": [
+//       "warn",
+//       { allowConstantExport: true },
+//     ],
+//     "quotes": [2, "double", { "avoidEscape": true }],
+//     "require-jsdoc": 0,
+//     "linebreak-style": 0,
+//     "react/prop-types": 0,
+//     "max-len": 0,
+//     "no-unused-vars": 0,
+//   },
+// }
--- frontend_p/src/pages/Home.tsx
+++ frontend_p/src/pages/Home.tsx
@@ -5,18 +5,27 @@ function Home() {
   return (
     <>
       <Navigation />
-      <main>
+      <main className="home-page-main">
         <h1>Meditation do be crazy</h1>
         <p>
-          We out here meditating... What you doing?<br />
-          Before you think about <b> killing yourself </b> with all those bitchass medicines,<br />
-          How about we talk about something romantic. <br />Like how good your mother looks tonight! <i>Anyways</i><br />
-          Try out our custom made meditation plans to ease you into not <b>killing yourself</b>.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+          suscipit?
           <br />
-
+          Before you think about <b> abusing all those opioids </b>
+          <br />
+          How about we talk about something romantic. <br />
+          Like how good your mother looks tonight! <i>Anyways</i>
+          <br />
+          Try out our custom made meditation plans to ease you into not{" "}
+          <b>abusing opioids</b>.
+          <br />
           <div className="redirect-buttons">
-            <Link className="browse-button" to="/browse">Take a look around</Link> or
-            <Link className="user-action-button" to="user-action">Join us</Link>
+            <Link className="browse-button" to="/login">
+              <button>Join us</button>
+            </Link>{" "}
+            <Link className="user-action-button" to="/browse">
+              <button>Take a Look Around</button>
+            </Link>
           </div>
         </p>
       </main>
